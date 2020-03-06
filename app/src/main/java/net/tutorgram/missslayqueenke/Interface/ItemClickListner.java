c06ee3d2b98b0502e0535d62b2f73612ff123aed
Don Shade <barackoginga@gmail.com>
2020-03-06 14:29:25 +0300

    So that if i loose my project i can get it back DonShade Miss Slay Queen App

---
+++ app/src/main/java/net/tutorgram/missslayqueenke/Interface/ItemClickListner.java
@@ -0,0 +1,8 @@
+package net.tutorgram.missslayqueenke.Interface;
+
+import android.view.View;
+
+public interface ItemClickListner
+{
+    void onClick(View view, int position, boolean isLongClick);
+}
---
+++ app/src/main/java/net/tutorgram/missslayqueenke/Prevalent/Prevalent.java
@@ -0,0 +1,9 @@
+package net.tutorgram.missslayqueenke.Prevalent;
+
+import net.tutorgram.missslayqueenke.Model.Users;
+
+public class Prevalent {
+    public static Users currentOnlineUser;
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+}
